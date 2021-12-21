63a41317c14a719660ba03e3d3ff99e2c458590e
Khoirul Asfian <45413544+asfian99@users.noreply.github.com>
2021-12-22 02:30:23 +0700

    (bug) update code edit transaksi

--- transaksi/edit.php
+++ transaksi/edit.php
@@ -19,10 +19,9 @@
     ociexecute($sqlMhs);
 
     $row = oci_fetch_array($sql, OCI_RETURN_NULLS + OCI_ASSOC);
-    var_dump($row);
   ?>
   <div class="card p-4 m-4" style="min-height: 100%; max-width: 50vw;">
-    <h3 class="mb-4">Edit Mahasiswa</h3>
+    <h3 class="mb-4">Edit Transaksi</h3>
 
     <!-- forms -->
     <form action="" method="post">
@@ -30,12 +29,12 @@
         <!-- waktu pinjam -->
         <div class="col mb-3 mx-2">
           <label for="pinjam" class="form-label">Waktu Pinjam</label>
-          <input required type="date" value="<?= $row['WAKTU_PINJAM'] ?>" class="form-control" id="pinjam" name="pinjam">
+          <input required type="date" value="<?= date_format(date_create($row['WAKTU_PINJAM']), "Y-m-d") ?>" class="form-control" id="pinjam" name="pinjam">
         </div>
         <!-- waktu kembali -->
         <div class="col mb-3 mx-2">
           <label for="kembali" class="form-label">Waktu Kembali</label>
-          <input required type="date" value="<?= $row['WAKTU_KEMBALI'] ?>" class="form-control" id="kembali" name="kembali">
+          <input required type="date" value="<?= date_format(date_create($row['WAKTU_KEMBALI']), "Y-m-d") ?>" class="form-control" id="kembali" name="kembali">
         </div>
       </div>
 
@@ -45,7 +44,7 @@
           <label for="mahasiswa" class="form-label">Mahasiswa</label>
           <select class="form-select" name="mahasiswa" id="mahasiswa">
             <?php while ($rowMhs = oci_fetch_array($sqlMhs, OCI_RETURN_NULLS + OCI_ASSOC)) { ?>
-              <option <?php $rowMhs['ID'] == $row['MAHASISWA_ID'] ? 'selected' : '' ?> value="<?= $rowMhs['ID'] ?>"> <?= $rowMhs['NAMA'] ?> </option>
+              <option <?= $rowMhs['ID'] == $row['MAHASISWA_ID'] ? 'selected' : '' ?> value="<?= $rowMhs['ID'] ?>"> <?= $rowMhs['NAMA'] ?> </option>
             <?php } ?>
           </select>
         </div>
@@ -54,7 +53,7 @@
         <label for="buku" class="form-label">Buku</label>
           <select class="form-select" name="buku" id="buku">
             <?php while ($rowBuku = oci_fetch_array($sqlBuku, OCI_RETURN_NULLS + OCI_ASSOC)) { ?>
-              <option <?php $rowBuku['ID'] == $row['BUKU_ID'] ? 'selected' : '' ?> value="<?= $rowBuku['ID'] ?>"> <?= $rowBuku['JUDUL'] ?> </option>
+              <option <?= $rowBuku['ID'] == $row['BUKU_ID'] ?'selected' : '' ?> value="<?= $rowBuku['ID'] ?>"> <?= $rowBuku['JUDUL'] ?> </option>
             <?php } ?>
           </select>
         </div>
@@ -70,19 +69,18 @@
 
   <?php 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      $nama = $_POST['nama'];
-      $nrp = $_POST['nrp'];
-      $angkatan = $_POST['angkatan'];
-      $kelas = $_POST['kelas'];
-      $notlp = $_POST['notlp'];
+      $pinjam = $_POST['pinjam'];
+      $kembali = $_POST['kembali'];
+      $mahasiswa = $_POST['mahasiswa'];
+      $buku = $_POST['buku'];
 
-      $query = ociparse($conn, "UPDATE PERPUSTAKAAN.MAHASISWA 
-                                SET NAMA = '$nama', 
-                                    NRP = '$nrp', 
-                                    ANGKATAN = '$angkatan', 
-                                    KELAS = '$kelas', 
-                                    NO_TELP = '$notlp'
-                                WHERE ID = $id");
+      echo "$id, $pinjam, $kembali, $mahasiswa, $buku";
+      $query = ociparse($conn, "UPDATE PERPUSTAKAAN.TRANSAKSI 
+                                SET BUKU_ID = $buku, 
+                                    MAHASISWA_ID = $mahasiswa, 
+                                    WAKTU_PINJAM = TO_DATE('$pinjam', 'YYYY-MM-DD'), 
+                                    WAKTU_KEMBALI = TO_DATE('$kembali', 'YYYY-MM-DD'), 
+                                WHERE PERPUSTAKAAN.TRANSAKSI.ID = $id");
       ociexecute($query);
 
       echo '<script type="text/javascript">
