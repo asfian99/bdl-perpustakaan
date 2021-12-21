b23257e6d9e9df97c3c4812a8a4181fbcb71ff48
Khoirul Asfian <45413544+asfian99@users.noreply.github.com>
2021-12-22 02:06:56 +0700

    add landing page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require('./layout/head.php') ?>
+<body style="background: #004C3F;">
+  <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="col-10 py-4 my-auto">
+      <div class="card mx-auto p-4" style="min-height: 100%; width: 50%;">
+        <h4 class="text-center m-0">Selamat Datang</h4>
+        <h2 class="text-center mb-5">SIM Perpustakaan</h2>
+        <a href="./buku/" class="btn btn-primary mx-auto" style="width: 20%;">Masuk</a>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,31 +1,46 @@
 <div class="col-2 py-4" style="min-height: 100vh;">
-  <h2 class="text-center text-white">SIM</h2>  
-  <h4 class="text-center text-white mb-5">PERPUSTAKAAN</h4>  
+  <div class="d-flex flex-column justify-content-between" style="height: 100%;">
+    <div>
+      <div>
+        <h2 class="text-center text-white">SIM</h2>  
+        <h4 class="text-center text-white mb-5">PERPUSTAKAAN</h4>  
+      </div>
 
-  <div class="list-group bg-main">
-    <!-- <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
-      The current link item
-    </a> -->
-    <a href="../buku/" class="list-group-item list-group-item-action bg-main">
-      <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-      </svg>
-      Buku
-    </a>
-    <a href="../mahasiswa/" class="list-group-item list-group-item-action bg-main">
-      <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path d="M12 14l9-5-9-5-9 5 9 5z" />
-        <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
-      </svg>
-      Mahasiswa
-    </a>
-    <a href="../transaksi/" class="list-group-item list-group-item-action bg-main">
-      <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-      </svg>
-      Transaksi
-    </a>
+      <div class="list-group bg-main">
+        <!-- <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+          The current link item
+        </a> -->
+        <a href="../buku/" class="list-group-item list-group-item-action bg-main">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+          </svg>
+          Buku
+        </a>
+        <a href="../mahasiswa/" class="list-group-item list-group-item-action bg-main">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path d="M12 14l9-5-9-5-9 5 9 5z" />
+            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+          </svg>
+          Mahasiswa
+        </a>
+        <a href="../transaksi/" class="list-group-item list-group-item-action bg-main">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+          </svg>
+          Transaksi
+        </a>
+      </div>
+    </div>
+
+    <div class="list-group bg-main">
+        <a href="../" class="list-group-item text-white fw-bold list-group-item-action bg-danger">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+          </svg>
+          Keluar
+        </a>
+    </div>
   </div>
 
 </div>
